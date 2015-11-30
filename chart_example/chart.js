5de55697d8542749327d7bcc781dd915e3bf53f4
HunterVoid96 <WEEB1_14@CH1001-017.uni.worc.ac.uk>
2015-11-30 11:35:42 +0000

    t4

--- chart_example/chart.js
+++ chart_example/chart.js
@@ -20,20 +20,36 @@ $(document).on("pagecreate", "#chartPage", function () {
       	title :{
       		text: "A random chart"
       	},
-      	axisX: {						
-      		title: "Random Values"
-      	},
-      	axisY: {						
-      		title: "Time (seconds)"
-      	},
-      	data: [{
-      		type: "line",
-      		dataPoints : dps
-      	}]
-   	});
-	
-	  
-});
+		data: [{        
+        type: "column",
+        dataPoints: [
+        { x: 10, y: 171 },
+        { x: 20, y: 155},
+        { x: 30, y: 150 },
+        { x: 40, y: 165 },
+        { x: 50, y: 195 },
+        { x: 60, y: 168 },
+        { x: 70, y: 128 },
+        { x: 80, y: 134 },
+        { x: 90, y: 114}
+        ]
+      },
+      {        
+        type: "column",
+        dataPoints: [
+        { x: 10, y: 71 },
+        { x: 20, y: 55},
+        { x: 30, y: 50 },
+        { x: 40, y: 65 },
+        { x: 50, y: 95 },
+        { x: 60, y: 68 },
+        { x: 70, y: 28 },
+        { x: 80, y: 34 },
+        { x: 90, y: 14}
+        ]
+      }        
+      ]
+    });
 
 function updateChart(random) {
       	updateChart.frequency = 3000;
